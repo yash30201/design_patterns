84b24d6ee4d1509a1c0529e0eae561d5644ddb75
yash30201 <54198301+yash30201@users.noreply.github.com>
2025-01-07 19:50:10 +0530

    feat: Add Factory method creational design pattern

---
+++ src/main/java/com/designpatterns/FactoryMethod.java
@@ -0,0 +1,63 @@
+package com.designpatterns;
+
+
+interface Product {
+    public void sayHello();
+}
+
+class ProductA implements Product {
+    @Override
+    public void sayHello() {
+        System.out.println("I'm product A");
+    }
+}
+
+class ProductB implements Product {
+    @Override
+    public void sayHello() {
+        System.out.println("I'm product B");
+    }
+}
+
+abstract class Client {
+    abstract protected Product createProduct();
+    
+    public void whichProduct() {
+        Product product = createProduct();
+        product.sayHello();
+    }
+}
+
+final class ClientA extends Client {
+    @Override
+    protected Product createProduct() {
+        return new ProductA();
+    }
+}
+
+final class ClientB extends Client {
+    @Override
+    protected Product createProduct() {
+        return new ProductB();
+    }
+}
+
+
+public class FactoryMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 4 ; ++i) {
+            Client client;
+            if (i % 2 == 0) {
+                client = new ClientA();
+            } else {
+                client = new ClientB();
+            }
+            client.whichProduct();
+        }
+    }
+
+}
